0fa51a025ef813e3ce62c4c14ee8fab608c60393
Goik <sbabinski@outlook.com>
2023-01-17 17:58:55 -0300

    Add assets
    Create states structure.
    Add DragonBall assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import "./app.css";
 import { CardsContainer } from "./components/CardsContainer";
 import { Control } from "./components/Control";
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentItemSelection, changeCurrentSelection] = useState([]);
+  const items = [];
+
+  for (let index = 0; index < 20; index++) {
+    items.push(index);
+  }
+
+  const selectedItems = [];
+
+  function selectItem(item) {
+    selectedItems.push(item);
+  }
+
   return (
     <main>
-      <Control />
-      <CardsContainer />
+      <Control
+        currentScore={currentScore}
+        topScore={topScore}
+        setTopScore={setTopScore}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
+      <CardsContainer
+        items={items}
+        currentItemSelection={currentItemSelection}
+        changeCurrentSelection={changeCurrentSelection}
+        selectItem={selectItem}
+        setCurrentScore={setCurrentScore}
+      />
     </main>
   );
 }
---
+++ src/components/CharacterList.js
@@ -0,0 +1,5 @@
+export const charList = [
+    {index: 0, name:"Android 16", img:"../assets/Android16.png"},
+    {index: 1, name:"Android 17", img:"../assets/Android17.png"},
+    {index: 2, name:"Android 18", img:"../assets/Android18.png"}
+]
